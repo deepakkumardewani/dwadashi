8b86ab067472e8f7443265136b5cc2e33b023e21
Deepak Dewani <deepak.dewani@accionlabs.com>
2020-02-19 17:44:10 +0545

    ADD: axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import MaterialUIPickers from './DateTimePicker'
 import './App.css';
-
+import moment from "moment";
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      start: moment().format('HH:mm A'),
+      end: moment().format('HH:mm A'),
+      date: moment().format('DD-MM-YYYY')
+    }
+  }
+
+  selectTime = (e, type, time) => {
+    this.setState({ [type] : time})
+  }
+  selectDate = (e, date) => {
+    this.setState({ date })
+  }
+
   render() {
     return (
-      <MaterialUIPickers />
+      <MaterialUIPickers
+        onTimeChange={this.selectTime}
+        onDateChange={this.selectDate}
+      />
     )
   }
 }
--- src/DateTimePicker.js
+++ src/DateTimePicker.js
@@ -1,5 +1,6 @@
 import 'date-fns';
-import React, { Fragment } from 'react';
+// import axios from 'axios';
+import React, { Fragment, Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import DateFnsUtils from '@date-io/date-fns';
@@ -8,60 +9,71 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import moment from "moment";
 
-export default function MaterialUIPickers() {
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
-  const handleDateChange = date => {
-    setSelectedDate(date);
-    test(date)
-  };
+export default class MaterialUIPickers extends Component {
 
-  return (
-    <Fragment>
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container justify="space-around">
-        <KeyboardDatePicker
-          margin="normal"
-          id="date-picker-dialog"
-          label="Date"
-          format="MM/dd/yyyy"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-        <KeyboardTimePicker
-          margin="normal"
-          id="time-picker"
-          label="Start Time"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time',
-          }}
-        />
-        <KeyboardTimePicker
-          margin="normal"
-          id="time-picker"
-          label="End Time"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time',
-          }}
-        />
-      </Grid>
-    </MuiPickersUtilsProvider>
-    <Button variant="contained" color="primary">
-        Submit button
-      </Button>
-      </Fragment>
-  );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dateTime: moment().format()
+    }
+  }
+
+  render() {
+    const { onTimeChange, onDateChange } = this.props;
+    return (
+      <Fragment>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog"
+            label="Date"
+            format="MM/dd/yyyy"
+            value={this.state.dateTime}
+            onChange={onDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="Start Time"
+            value={this.state.dateTime}
+            onChange={(e, date) => onTimeChange(e, 'start', date)}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="End Time"
+            value={this.state.dateTime}
+            onChange={(e, date) => onTimeChange(e, 'end', date)}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+      <Button variant="contained" color="primary">
+          Submit button
+        </Button>
+        </Fragment>
+    );
+  }
 }
 
+// function test(selectedDate) {
+//   console.log({selectedDate});
 
-function test(selectedDate) {
-  console.log(selectedDate);
-}
+//   axios.get(`http://localhost:3050/testing`)
+//       .then(res => {
+//         console.log({res});
+//       })
+// }
